2045567d4b4625d2c84b2cc447e28d1777183d1b
Joshua Suchit <86627712+Josh-S0@users.noreply.github.com>
2022-02-08 15:09:46 +0000

    Initial project structure and MongoConnector creation

---
+++ Database/MongoConnector.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+
+namespace CSUserAPI.Database
+{
+	//singleton class so only one instance is running at a time
+    public sealed class MongoConnector
+    {
+		private static MongoConnector? single_instance = null;
+		private static readonly object padlock = new object();
+		private IMongoDatabase mongoDb;
+		
+		public MongoClient mongoClient;
+
+		private MongoConnector()
+		{
+			this.mongoClient = new MongoClient("mongodb://localhost:27017");
+			this.mongoDb = mongoClient.GetDatabase("MainDB");
+		}
+		
+		public static MongoConnector getInstance
+		{
+            get{
+				lock (padlock)
+                {
+					if (single_instance == null)
+						{
+							single_instance = new MongoConnector();
+						}
+					return single_instance;
+                }
+            }
+			
+		}
+
+		
+	}
+}
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CSUserAPI.Models
+{
+    public class User
+    {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string UserId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string password { get; set; }
+        
+
+
+    }
+}
