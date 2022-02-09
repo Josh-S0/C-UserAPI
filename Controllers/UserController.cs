dd7f0a41bda551bff8d21c229ad31421d41a49e6
Joshua Suchit <86627712+Josh-S0@users.noreply.github.com>
2022-02-09 14:57:48 +0000

    Controller updated for correct url routing

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,10 +6,10 @@ using MongoDB.Driver;
 namespace CSUserAPI.Controllers
 {
     [ApiController]
-    [Route("api/User")]
-    public class UserController : Controller
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
     {
-        private IMongoCollection<User> userCollection;
+        private readonly IMongoCollection<User> userCollection;
 
         public UserController(IMongoClient client)
         {
@@ -17,31 +17,31 @@ namespace CSUserAPI.Controllers
            userCollection = database.GetCollection<User>("Users");
         }
 
-        [HttpGet("/get")]
+        [HttpGet("get")]
         public List<User> GetUsers()
         {
             return userCollection.Find(new BsonDocument()).ToList();
         }
-        [HttpGet("/get/{id}")]
+        [HttpGet("get/{id}")]
         public User GetById(String id)
         {
             var idFilter = Builders<User>.Filter.Eq("userId", id);
             return userCollection.Find(idFilter).FirstOrDefault();
         }
-        [HttpGet("/search")]
+        [HttpGet("search")]
         public User GetByCriteria(String criteria, String query)
         {
             var criteriaFilter = Builders<User>.Filter.Eq(criteria,query);
             return userCollection.Find(criteriaFilter).FirstOrDefault();       
         }
-        [HttpDelete("/delete/{id}")]
+        [HttpDelete("delete/{id}")]
         public void DeleteById(String id)
         {
             var deleteFilter = Builders<User>.Filter.Eq("userId", id);
             userCollection.DeleteOne(deleteFilter);
         }
        
-        [HttpPost("/add")]
+        [HttpPost("add")]
         public void AddUser(User user)
         {
             user.userId = Guid.NewGuid().ToString();
